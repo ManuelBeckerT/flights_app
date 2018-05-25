033e4b70e93b07f2960634bcf8a8d8d56a56e8cf
Manuel Becker <mabecker@uc.cl>
2018-05-24 20:00:44 -0400

    Funcionando

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'maps/index'
-
-  get 'urls/index'
+  root :to => "maps#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
